22771349630e67cb0f691a88e99b45a5d36e17ec
David R. Jex <david.jex@me.com>
2017-08-27 13:38:12 -0500

    Intitial push

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//Create a new component. This component should produce so HTML
+//const declares a constant which is a steady variable that does 
+//not change
+const App = () => {
+	return <div>Hi!<div>;
+	//the <div> is JSX a subset or dialect of javascript that allows
+	//us to write what looks like HTML but is javascript
+}
+//Take this component's generated HTML and put it on the page 
+//(in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
